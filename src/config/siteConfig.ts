6f9adf3721ac5121f039fa74902b62bb3b28d7b0
yxss2483 <2483949464@qq.com>
2026-07-05 18:27:30 +0800

    config:修改配置

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,7 +6,7 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "alyxia's Blog",
 
 	// 站点副标题
 	subtitle: "Demo site",
@@ -78,7 +78,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "alyxia's Blog",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
